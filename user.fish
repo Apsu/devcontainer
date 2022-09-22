8399e82c4e7f0dcabd4aca859dbf5dbb32a0112d
Evan Callicoat <apsu@propter.net>
2022-09-22 12:27:19 -0500

    Tweaks

--- user.fish
+++ user.fish
@@ -1,6 +1,8 @@
 # Env vars
 set -Ux TERM xterm-256color
 set -Ux LANG en_US.UTF-8
+# Other exports
+set -Ux ADSK_USER_NAME callice
 
 # Setup pyenv
 set -Ux PYENV_ROOT "$HOME/.pyenv"
@@ -17,6 +19,3 @@ fish_add_path -Up /opt/plangrid/
 
 # User bin path (python/etc)
 fish_add_path -Up $HOME/.local/bin
-
-# Other exports
-set -Ux ADSK_USER_NAME callice
