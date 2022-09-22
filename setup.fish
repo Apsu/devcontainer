85f73c435298c7b4b190a70b849f5f3a15a613a7
Evan Callicoat <evan.callicoat@autodesk.com>
2022-09-22 00:51:49 -0500

    Tweak a bit

--- setup.fish
+++ setup.fish
@@ -1,35 +1,22 @@
 #!/usr/bin/env fish
 
-# Install pyenv
-curl https://pyenv.run | bash
-# Setup pyenv
-set -Ux PYENV_ROOT "$HOME/.pyenv"
-fish_add_path -Up $PYENV_ROOT/bin
-pyenv init - fish | source
-pyenv virtualenv-init - fish | source
-# Install python 3.9.4
-pyenv install 3.9.4
-
-# Install tfenv/tgenv
-git clone https://github.com/tfutils/tfenv.git ~/.tfenv
-git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
-
-# Install awscli
-pip install awscli mssh
-
 # Setup local bin
 mkdir -p /home/dev/.local/bin
 
 # Setup workspace
 mkdir -p ~/workspace
-cd ~/workspace
+
+# Install tfenv/tgenv
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
 
 # Clone work repos
-git clone https://github.com/plangrid/dacloud-terraform
-git clone https://github.com/plangrid/dacloud-tf-build-tools
-git clone https://github.com/plangrid/plangrid-devops
-git clone https://github.com/plangrid/spinnaker-tools
-git clone https://github.com/plangrid/onboarding
+cd ~/workspace
+git clone https://github.com/plangrid/dacloud-terraform.git
+git clone https://github.com/plangrid/dacloud-tf-build-tools.git
+git clone https://github.com/plangrid/plangrid-devops.git
+git clone https://github.com/plangrid/spinnaker-tools.git
+git clone https://github.com/plangrid/onboarding.git
 
 # Setup TF build tools
 cd ~/workspace/dacloud-tf-build-tools
@@ -39,3 +26,19 @@ make setup
 # Setup AWS creds tool
 cd ~/workspace/onboarding
 make get-aws-creds
+
+# Go home
+cd
+
+# Install awscli
+pip install awscli mssh
+
+# Install pyenv
+curl https://pyenv.run | bash
+# Setup pyenv
+set -Ux PYENV_ROOT "$HOME/.pyenv"
+fish_add_path -Up $PYENV_ROOT/bin
+pyenv init - fish | source
+pyenv virtualenv-init - fish | source
+# Install python 3.9.4
+pyenv install 3.9.4
